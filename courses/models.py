2d082ae8f3dcbb7146a6fbc4c78c02005334731b
mortif <mortif3767@outlook.com>
2017-11-06 14:48:28 +0800

    models build

--- courses/models.py
+++ courses/models.py
@@ -1,32 +1,77 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 
 class Subject(models.Model):
-	title = models.CharField(max_length=200)
-	slug = models.SlugField(max_length=200, unique=True)
-	class Meta:
-		ordering = ('title',)
-	def __str__(self):
-		return self.title
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    class Meta:
+        ordering = ('title',)
+    def __str__(self):
+        return self.title
 
 
 class Course(models.Model):
-	owner = models.ForeignKey(User, related_name='courses_created')
-	subject = models.ForeignKey(Subject, related_name='courses')
-	title = models.CharField(max_length=200)
-	slug = models.CharField(max_length=200, unique=True)
-	overview = models.TextField()
-	created = models.DateTimeField(auto_now_add=True)
-	class Meta:
-		ordering = ('-created',)
-	def __str__(self):
-		return self.title
+    owner = models.ForeignKey(User, related_name='courses_created')
+    subject = models.ForeignKey(Subject, related_name='courses')
+    title = models.CharField(max_length=200)
+    slug = models.CharField(max_length=200, unique=True)
+    overview = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        ordering = ('-created',)
+    def __str__(self):
+        return self.title
 
 
 class Module(models.Model):
-	course = models.ForeignKey(Course, related_name='modules')
-	title = models.CharField(max_length=200)
-	description = models.TextField(blank=True)
-	def __str__(self):
-		return self.title
+    course = models.ForeignKey(Course, related_name='modules')
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    def __str__(self):
+        return self.title
+
+
+class Content(models.Model):
+    module = models.ForeignKey(Module, related_name='contents')
+    content_type = models.ForeignKey(ContentType,
+                                     limit_choices_to={'model__in':
+                                     ('text', 'video', 'image', 'file')})
+    object_id = models.PositiveIntegerField()
+    item = GenericForeignKey('content_type', 'object_id')
+'''
+我们需要三种不同的字段来设置一个通用关系。在我们的Content模型中，它们是：
+
+    content_type：一个ForeignKey字段指向ContentType模型
+    object_id：这是PositiveIntegerField用来存储有关联对象的关键字
+    item：一个GenericForeignKey字段指向被关联的对象通过结合前两个字段
+'''
+
+
+class ItemBase(models.Model):
+    owner = models.ForeignKey(User, related_name='%(class)s_related')
+    #django支持扩展类名自动匹配,exp: text_related
+    title = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.title
+
+
+class Text(ItemBase):
+    content = models.TextField()
+
+class File(ItemBase):
+    file = models.FileField(upload_to='files')
+
+class Image(ItemBase):
+    file = models.FileField(upload_to='images')
+
+class Video(ItemBase):
+    url = models.URLField()
